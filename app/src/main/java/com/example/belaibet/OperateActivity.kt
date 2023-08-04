6f65a8475a0c4308278c686396968e3488df509e
Ivan <vanec98@mail.ru>
2023-08-04 12:18:56 +0300

    initial_commit

---
+++ app/src/main/java/com/example/belaibet/OperateActivity.kt
@@ -0,0 +1,14 @@
+package com.example.belaibet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OperateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
